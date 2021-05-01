2e9e1a8a6d2a625f41a134eb00ada566ad917dc1
Joachim Von Tittentrop <harriet@goballs.gov>
2021-05-01 16:43:04 -0500

    first successful login

--- tc/tc.go
+++ tc/tc.go
@@ -1,9 +1,12 @@
 package tc
 
-import "net/http"
-import "net/url"
-import "log"
-import "io"
+import (
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+)
 
 const TcHost = "https://tinychat.com"
 
@@ -13,9 +16,56 @@ type tcClient struct {
 }
 
 func Login(username string, password string) tcClient {
-	var payload = makeLoginForm(username, password)
+
+	const redirectUrl = TcHost + "/home"
+
+	doc, cookies := loadSignOnData(redirectUrl)
+
+	token := parseLoginToken(doc)
+
+	request := buildLoginRequest(username, password, token, cookies)
+	client := &http.Client{}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Panic("shit")
+	}
+
+	defer resp.Body.Close()
+
+	//body, err := io.ReadAll(resp.Body)
+	//if err != nil {
+	//	log.Panic("error happened")
+	//}
+
+	//bodyStr := string(body)
+	//log.Print("result: " + bodyStr)
+
+	log.Print("-status: " + resp.Status)
+
+	return tcClient{username, make(map[string]string)}
+}
+
+func buildLoginRequest(username string, password string, token string, cookies []*http.Cookie) *http.Request {
 	const url = TcHost + "/login"
-	resp, err := http.PostForm(url, payload)
+	formData := makeLoginForm(username, password, token)
+	formDataEncoded := formData.Encode()
+	request, err := http.NewRequest("POST", url, strings.NewReader(formDataEncoded))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	log.Print("request: " + formDataEncoded)
+	if err != nil {
+		log.Panic("error happened")
+	}
+	for _, cookie := range cookies {
+		request.AddCookie(cookie)
+	}
+
+	return request
+}
+
+func loadSignOnData(redirect string) (*goquery.Document, []*http.Cookie) {
+	url := TcHost + "/start?" + redirect
+	resp, err := http.Get(url)
 
 	if err != nil {
 		log.Panic("error happened")
@@ -23,23 +73,29 @@ func Login(username string, password string) tcClient {
 
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		log.Panic("error happened")
 	}
-
-	bodyStr := string(body)
-	log.Print("result: " + bodyStr)
-
-	log.Print("status: " + resp.Status)
-
-	return tcClient{username, make(map[string]string)}
+	return doc, resp.Cookies()
 }
 
-func makeLoginForm(username string, password string) url.Values {
+func parseLoginToken(doc *goquery.Document) string {
+	tokenNode := doc.Find("#form-signin > input[name='_token']")
+	token, exists := tokenNode.Attr("value")
+
+	if !exists {
+		log.Panic("token not found")
+	}
+	return token
+}
+
+func makeLoginForm(username string, password string, token string) url.Values {
 	return url.Values{
 		"login_username": {username},
 		"login_password": {password},
 		"remember":       {"1"},
+		"next":           {"https://tinychat.com/home"},
+		"_token":         {token},
 	}
 }
